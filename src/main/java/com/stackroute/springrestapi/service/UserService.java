5abd4c605678e24dd81d749589736019d06c3b6f
dikshab9 <dikshabaruah79@gmail.com>
2017-10-16 10:21:52 +0530

    service commit

--- src/main/java/com/stackroute/springrestapi/service/UserService.java
+++ src/main/java/com/stackroute/springrestapi/service/UserService.java
@@ -1,5 +1,18 @@
 package com.stackroute.springrestapi.service;
 
+import java.util.List;
+
+
+import com.stackroute.springrestapi.domain.UserModel;
+
+
+
 public interface UserService {
+	public List<UserModel> getAllUsers( );
+	public UserModel addUser(UserModel usermodel);
+	public UserModel updateUser(UserModel usermodel);
+	public int getUserbyId(int id);
+    public boolean delete(int id);
+	
 
 }
