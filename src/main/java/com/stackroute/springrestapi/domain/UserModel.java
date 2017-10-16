7d4b39eee8f4683b97fa9b8c15e0696a1c731fdd
dikshab9 <dikshabaruah79@gmail.com>
2017-10-16 10:17:04 +0530

    model

--- src/main/java/com/stackroute/springrestapi/domain/UserModel.java
+++ src/main/java/com/stackroute/springrestapi/domain/UserModel.java
@@ -1,5 +1,34 @@
 package com.stackroute.springrestapi.domain;
 
 public class UserModel {
+	private int id;
+	private String username;
+	private String email_id;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getEmail_id() {
+		return email_id;
+	}
+	public void setEmail_id(String email_id) {
+		this.email_id = email_id;
+	}
+	public UserModel(int id, String username, String email_id) {
+		super();
+		this.id = id;
+		this.username = username;
+		this.email_id = email_id;
+	}
+	public UserModel() { 
 
+}
 }
